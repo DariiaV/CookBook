fc1c2b9c9f68a9de106d9bdbdf908acc0395d933
Siarheimasiuk <winst73@gmail.com>
2022-11-28 23:46:10 +0300

    clean cod

---
+++ CookBook/CookBook/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CookBook
+//
+//  Created by Дария Григорьева on 27.11.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vieController = ViewController()
+        window?.rootViewController = vieController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
