4e73f32d01ba546f1a9c752588a1279361dc58c4
KhazovDV <101877272+KhazovDV@users.noreply.github.com>
2022-11-29 19:55:36 +0300

    Added Name3 screen.

--- CookBook/CookBook/Sources/Team Screen/Name3.swift
+++ CookBook/CookBook/Sources/Team Screen/Name3.swift
@@ -1,8 +1,35 @@
-//
-//  Name.swift
-//  CookBook
-//
-//  Created by Дария Григорьева on 28.11.2022.
-//
 
-import Foundation
+import UIKit
+
+class Name3: UIViewController {
+    let nextButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        setupButton()
+        view.backgroundColor = .systemBackground
+        title = "First Screen"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        //        createView1Constraint()
+    }
+//    func setupButton() {
+//        view.addSubview(nextButton)
+//
+//        nextButton.configuration = .filled()
+//        nextButton.configuration?.baseBackgroundColor = .systemCyan
+//        nextButton.configuration?.title = "Next"
+//
+//        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+//
+//        nextButton.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            nextButton.widthAnchor.constraint(equalToConstant: 200),
+//            nextButton.heightAnchor.constraint(equalToConstant: 50)])
+//
+    }
+
+
+
+
