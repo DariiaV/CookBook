19172298da44b2309f24724050007038569c797a
KhazovDV <101877272+KhazovDV@users.noreply.github.com>
2022-11-30 21:16:17 +0300

    added button

--- CookBook/CookBook/Sources/Team Screen/Name3.swift
+++ CookBook/CookBook/Sources/Team Screen/Name3.swift
@@ -1,35 +1,35 @@
-
 import UIKit
 
 class Name3: UIViewController {
     let nextButton = UIButton()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupButton()
+        setupButton()
         view.backgroundColor = .systemBackground
         title = "First Screen"
         navigationController?.navigationBar.prefersLargeTitles = true
         //        createView1Constraint()
     }
-//    func setupButton() {
-//        view.addSubview(nextButton)
-//
-//        nextButton.configuration = .filled()
-//        nextButton.configuration?.baseBackgroundColor = .systemCyan
-//        nextButton.configuration?.title = "Next"
-//
-//        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
-//
-//        nextButton.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            nextButton.widthAnchor.constraint(equalToConstant: 200),
-//            nextButton.heightAnchor.constraint(equalToConstant: 50)])
-//
+    func setupButton() {
+        view.addSubview(nextButton)
+        
+        nextButton.backgroundColor = .systemRed
+        
+        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)])
+        
     }
-
-
-
+    
+    @objc func goToNextScreen() {
+        let nextScreen = HomeViewController()
+        nextScreen.title = "Second Screen"
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
+}
 
