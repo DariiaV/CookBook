7631b532dbcae3568c3aba93a709ece0b458061e
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2022-12-04 15:47:26 +0400

    Сreate icon upload project and correct launchScreen

--- CookBook/CookBook/Sources/NetworkManager/RecipeManager.swift
+++ CookBook/CookBook/Sources/NetworkManager/RecipeManager.swift
@@ -30,8 +30,8 @@ struct RecipeManager {
     var delegate: RecipeManagerDelegate?
     
     private let cache = NSCache<NSString, UIImage>()
-    private let recipeURL = "https://api.spoonacular.com/recipes/%@/information?includeNutrition=false&apiKey=YOU_API_KEY"
-    private let cuisinesURL = "https://api.spoonacular.com/recipes/complexSearch?Cuisines=%@&apiKey=YOU_API_KEY"
+    private let recipeURL = "https://api.spoonacular.com/recipes/%@/information?includeNutrition=false&e6ffd13c724e49f49a0a32615528c596"
+    private let cuisinesURL = "https://api.spoonacular.com/recipes/complexSearch?Cuisines=%@&e6ffd13c724e49f49a0a32615528c596"
     
     func fetchDetailRecipe(id: String?) {
         guard let id else {
