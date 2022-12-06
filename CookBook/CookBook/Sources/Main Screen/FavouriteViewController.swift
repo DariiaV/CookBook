50fa0eeaa40b2ab50db28e235da9da701397156c
KhazovDV <101877272+KhazovDV@users.noreply.github.com>
2022-12-06 20:13:13 +0300

    Added table in FavouriteViewController.

--- CookBook/CookBook/Sources/Main Screen/FavouriteViewController.swift
+++ CookBook/CookBook/Sources/Main Screen/FavouriteViewController.swift
@@ -1,10 +1,49 @@
 import UIKit
 
-class FavouriteViewController: UIViewController {
+class FavouriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var myTableView = UITableView()
+    let indentifire = "MyCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createTable()
         view.backgroundColor = .systemPurple
     }
+    func createTable() {
+        self.myTableView = UITableView(frame: view.bounds, style: .plain)
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: indentifire)
+        self.myTableView.delegate = self
+        self.myTableView.dataSource = self
+        myTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(myTableView)
+    }
+    //MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return 5
+        case 2:
+            return 8
+        default:
+            break
+        }
+        return 0
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: indentifire, for: indexPath)
+        cell.textLabel?.text = "section = \(indexPath.section) cell = \(indexPath.row)"
+        return cell
+    }
+    //MARK: - UITableViewDelegate
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
 }
+
