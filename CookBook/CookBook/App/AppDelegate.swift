4f6e896273dbedf2e89a14a9c2593ecd7059e431
Dariia <dariia.grigoreva@gmail.com>
2022-12-11 00:51:52 +0400

    fix launch screen

--- CookBook/CookBook/App/AppDelegate.swift
+++ CookBook/CookBook/App/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var favoriteRecipes: [CuisineRecipe] = []
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // add sleep to show launch screen
+        Thread.sleep(forTimeInterval: 3.0)
         window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = FirstScreenViewController()
         let navController = UINavigationController (rootViewController: viewController)
